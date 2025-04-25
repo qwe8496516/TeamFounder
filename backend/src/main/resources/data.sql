d54a7383121858150d2e981f6b664120a832e269
t113598012 <t113598012@ntut.edu.tw>
2025-04-25 22:38:01 +0800

    update: sql scripts & spring boot setting

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+# Users
+INSERT IGNORE INTO users (userId, username, password, email, privilege, createdAt) VALUES ('113598012', 'Yang Hong Jie', 123, 't113598012@ntut.org.tw', 0, '2025-04-25 05:38:20');
+INSERT IGNORE INTO users (userId, username, password, email, privilege, createdAt) VALUES ('113598056', 'CK Yang', 123, 't113598056@ntut.org.tw', 1, '2025-04-25 05:38:21');
