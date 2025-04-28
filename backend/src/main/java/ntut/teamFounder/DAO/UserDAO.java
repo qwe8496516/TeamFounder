24430d286171bffa2d145a144c4a28b2b61e4403
Eddie Yang <kazckyang@gmail.com>
2025-04-28 17:10:28 +0800

    i push you push everybody push

--- backend/src/main/java/ntut/teamFounder/DAO/UserDAO.java
+++ backend/src/main/java/ntut/teamFounder/DAO/UserDAO.java
@@ -49,30 +49,4 @@ public class UserDAO {
         return jdbcTemplate.update(sql, userId, username, password, email, 0);
     }
 
-    public int addSkillToUser(Long userId, Long skillId) {
-        String sql = "INSERT INTO profile (userId, skillId) VALUES (?, ?)";
-        return jdbcTemplate.update(sql, userId, skillId);
-    }
-
-    public List<Long> getSkillsByUserId(Long userId) {
-        String sql = "SELECT skillId FROM profile WHERE userId = ?";
-        return jdbcTemplate.query(sql, new Object[]{userId}, (rs, rowNum) ->
-                rs.getLong("skillId")
-        );
-    }
-
-    public int enrollInCourse(Long userId, String courseCode) {
-        String sql = "INSERT INTO enrollment (userId, course_code) VALUES (?, ?)";
-        return jdbcTemplate.update(sql, userId, courseCode);
-    }
-
-    public List<String> getCoursesByUserId(Long userId) {
-        String sql = "SELECT c.course_code FROM course c " +
-                "JOIN enrollment e ON c.course_code = e.course_code " +
-                "WHERE e.userId = ?";
-        return jdbcTemplate.query(sql, new Object[]{userId}, (rs, rowNum) ->
-                rs.getString("course_code")
-        );
-    }
-
 }
