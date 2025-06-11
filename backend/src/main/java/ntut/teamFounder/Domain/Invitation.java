40df061a65fecb3f7f2b281585ed9f744548d470
Eddie Yang <kazckyang@gmail.com>
2025-06-11 22:05:25 +0800

    remove comment

--- backend/src/main/java/ntut/teamFounder/Domain/Invitation.java
+++ backend/src/main/java/ntut/teamFounder/Domain/Invitation.java
@@ -9,14 +9,13 @@ import java.util.Map;
 @Getter
 @Setter
 public class Invitation {
-    // Getters and Setters
     private Long id;
     private Long senderId;
     private Long receiverId;
     private String courseCode;
     private String message;
     private int status;
-    
+
     public Map<String, Object> toMap() {
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("id", id);
